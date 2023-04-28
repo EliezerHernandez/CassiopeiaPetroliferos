4e7360bae21997bdbe266493e0e1637622a63c57
Eliezer Hernández <eliezer_hdz@outlook.com>
2023-04-28 15:01:02 -0600

    changed gatsby config directory

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,56 +1,32 @@
-import type { GatsbyConfig } from "gatsby";
+import { config } from "process";
 
-const config: GatsbyConfig = {
-  siteMetadata: {
-    title: 'Cassiopeia Petrolíferos',
+module.exports = {
+	siteMetadata: {
+		title: 'Cassiopeia Industries',
 		description: 'Lema de la empresa.',
 		image: '/src/images/logocompleto.jpeg',
 		siteUrl: 'https://www.cassiopeia.com.mx/',
-  },
-  graphqlTypegen: true,
-  plugins: [
-    "gatsby-plugin-postcss",
-    "gatsby-plugin-image", 
-    "gatsby-plugin-sitemap", 
-    {
-      resolve: 'gatsby-plugin-manifest',
-      options: {
-        "icon": "src/images/icon.png"
-      }
-    }, 
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sharp", 
-    "gatsby-transformer-sharp", 
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-        "name": "images",
-        "path": "./src/images/"
-      },
-      __key: "images"
-    },
-    // {
-    //   resolve: "gatsby-plugin-google-analytics",
-    //   options: {
-    //     trackingId: "",
-    //   },
-    // },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `pages`,
-        path: `./src/pages/`,
-      },
-    },
-    // {
-		// 	resolve: 'gatsby-source-filesystem',
-		// 	options: {
-		// 		name: 'gallery',
-		// 		path: './src/gallery/',
-		// 	},
-		// 	__key: 'gallery',
+	},
+	plugins: [
+		'gatsby-plugin-postcss',
+		'gatsby-plugin-image',
+		// {
+		//   resolve: "gatsby-plugin-google-analytics",
+		//   options: {
+		//     trackingId: "",
+		//   },
 		// },
-    {
+		'gatsby-plugin-react-helmet',
+		'gatsby-plugin-sitemap',
+		{
+			resolve: 'gatsby-plugin-manifest',
+			options: {
+				icon: 'src/images/icon.png',
+			},
+		},
+		'gatsby-plugin-sharp',
+		'gatsby-transformer-sharp',
+		{
 			resolve: 'gatsby-plugin-web-font-loader',
 			options: {
 				google: {
@@ -58,7 +34,21 @@ const config: GatsbyConfig = {
 				},
 			},
 		},
-  ]
-};
-
-export default config;
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'images',
+				path: '/src/images/',
+			},
+			__key: 'images',
+		},
+		{
+			resolve: 'gatsby-source-filesystem',
+			options: {
+				name: 'gallery',
+				path: '/src/gallery/',
+			},
+			__key: 'gallery',
+		},
+	],
+};export default config;
