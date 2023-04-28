f12882dd3d5073ae1d7bd2355d6cc017dd384da9
Eliezer Hernández <eliezer_hdz@outlook.com>
2023-04-28 15:32:36 -0600

    made visor better, added accesible gallery controls

--- src/components/common/Gallery.tsx
+++ src/components/common/Gallery.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { GatsbyImage, getImage } from 'gatsby-plugin-image';
 import type { IGatsbyImageData } from 'gatsby-plugin-image';
-
 interface Props {
 	images: {
 		thumb: IGatsbyImageData;
@@ -9,47 +8,75 @@ interface Props {
 	}[];
 	imageClass?: string;
 }
-
 export const Gallery: React.FunctionComponent<Props> = ({
 	images,
 	imageClass,
 }) => {
 	const [isVisorOpen, setIsVisorOpen] = React.useState(false);
-	const [currentImage, setCurrentImage] = React.useState<IGatsbyImageData>();
+	const [currentImageIndx, setCurrentImageIndx] = React.useState<number>();
+	const visorBg = React.useRef();
 
-	const openImage = (imgToOpen: IGatsbyImageData) => {
-		setCurrentImage(imgToOpen);
+	const openImage = (index: number) => {
+		setCurrentImageIndx(index);
 		setIsVisorOpen(true);
 	};
 
 	const closeImage = () => {
-		setCurrentImage(undefined);
+		setCurrentImageIndx(undefined);
 		setIsVisorOpen(false);
 	};
 
 	return (
 		<>
 			{isVisorOpen && (
-				<div className="bg-black/75 fixed top-0 left-0 bottom-0 right-0 z-20 flex flex-col py-8 px-inside">
+				<div
+					className="bg-black/75 fixed top-0 left-0 bottom-0 right-0 z-20 flex flex-col py-8 px-inside"
+					onClick={(e) => {
+						if (e.target == visorBg.current) {
+							closeImage();
+						}
+					}}
+					ref={visorBg}>
 					<div className="pb-4 text-white">
-						<p className="cursor-pointer font-bold" onClick={closeImage}>
+						<button className="font-bold text-lg" onClick={closeImage}>
 							Cerrar foto
-						</p>
+						</button>
 					</div>
-					<GatsbyImage image={currentImage} alt="" />
+					<GatsbyImage image={images[currentImageIndx].full} alt="" />
 					<div className="pt-4 text-white flex justify-between">
-						<p className="cursor-pointer font-bold">&larr; Anterior foto</p>
-						<p className="cursor-pointer font-bold">Siguiente foto &rarr;</p>
+						{currentImageIndx - 1 > 0 ? (
+							<button
+								className="font-bold text-lg"
+								onClick={() => setCurrentImageIndx((prev) => prev - 1)}>
+								&larr; Anterior foto
+							</button>
+						) : (
+							<span></span>
+						)}
+						{currentImageIndx + 1 < images.length && (
+							<button
+								className="font-bold text-lg"
+								onClick={() => setCurrentImageIndx((prev) => prev + 1)}>
+								Siguiente foto &rarr;
+							</button>
+						)}
 					</div>
 				</div>
 			)}
 
 			{images.map((img, i) => (
 				<GatsbyImage
+					tabIndex={0}
 					key={i}
-					image={getImage(img.thumb)}
+					image={img.thumb}
 					alt=""
-					onClick={() => openImage(img.full)}
+					onKeyPress={(e) => {
+						if (e.code == 'Space' || e.code == 'Enter') {
+							e.preventDefault();
+							openImage(i);
+						}
+					}}
+					onClick={() => openImage(i)}
 					className={imageClass}
 				/>
 			))}
