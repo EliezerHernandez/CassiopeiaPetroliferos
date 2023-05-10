4f14bcbf386a091bb359a296aa6d77b7d745d233
Eliezer Hernández <eliezer_hdz@outlook.com>
2023-05-10 17:45:57 -0600

    Se agregaron las entidades federativs

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as mailClient from 'emailjs-com';
-import { EntidadesFederativas } from './utils/EntidadesFederativas';
+import { EntidadesFederativas } from './utils/EntidadesFederativas'
 import { Spinner } from './common/Spinner';
 
 export const ContactForm: React.FunctionComponent = ({}) => {
